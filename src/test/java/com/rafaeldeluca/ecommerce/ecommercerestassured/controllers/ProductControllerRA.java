e48c74fe24f6e21eea2cb7bd45da5d4265d00972
Rafael de Luca <deluca1712@gmail.com>
2023-09-17 20:26:05 -0300

    find all when product name is not empty

--- src/test/java/com/rafaeldeluca/ecommerce/ecommercerestassured/controllers/ProductControllerRA.java
+++ src/test/java/com/rafaeldeluca/ecommerce/ecommercerestassured/controllers/ProductControllerRA.java
@@ -8,12 +8,14 @@ import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 public class ProductControllerRA {
     private Long existingProductId, nonExistingProductId;
+    private String productName;
 
     @BeforeEach
     public void setUp () {
         baseURI = "http://localhost:8080";
         existingProductId = 3L;
         nonExistingProductId = 500L;
+        productName = "Rails for Dummies";
 
     }
 
@@ -39,5 +41,32 @@ public class ProductControllerRA {
                 .get("/products/{id}", nonExistingProductIdInteger)
          .then()
                 .statusCode(404);
+    }
+
+    @Test
+    void findAllShouldReturnPageWithAllProductsWhenProductNameIsEmpty () {
+        given()
+                .get("products?size=25&page=0&sort=id")
+        .then()
+                .statusCode(200)
+                .body("content.name", hasItems("The Lord of the Rings", "Smart TV","Macbook Pro"))
+                .body("last", is(true))
+                .body("totalPages", is(1))
+                .body("totalElements", is(25))
+                .body("pageable.sort.sorted", is(true));
+    }
+
+    @Test
+    void findAllShouldReturnPageWithSomeProductsWhenProductNameIsNotEmpty () {
+        // pode retornar mais de um elemento, por isso  uso o index zero para
+        // comparar os dados do primeiro elemento retornado
+        given()
+                .get("/products?name={productName}", productName)
+        .then()
+                .statusCode(200)
+                .body("content.id[0]", is(5))
+                .body("content.price[0]", is(100.99F))
+                .body("content.name[0]", equalTo("Rails for Dummies"))
+                .body("content.imgUrl[0]", equalTo("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/5-big.jpg"));
     }
 }

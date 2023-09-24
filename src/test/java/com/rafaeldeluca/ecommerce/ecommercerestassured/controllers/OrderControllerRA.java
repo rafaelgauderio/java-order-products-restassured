1064f3a1954b4403d90852cbffea240fa4fc6f65
Rafael de Luca <deluca1712@gmail.com>
2023-09-24 19:07:42 -0300

    test order by id - existing order id and client logged and order belows to client

--- src/test/java/com/rafaeldeluca/ecommerce/ecommercerestassured/controllers/OrderControllerRA.java
+++ src/test/java/com/rafaeldeluca/ecommerce/ecommercerestassured/controllers/OrderControllerRA.java
@@ -51,5 +51,26 @@ public class OrderControllerRA {
                 .body("total", is(1431.0F));
     }
 
+    @Test
+    void findByIdShouldReturnOrderWhenOrderIdExistsAndUserLoggedAsClientAndOrderBelowsToClient () {
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + clientToken)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/orders/{id}", existingOrderId)
+                .then()
+                .statusCode(200)
+                .body("id", is(1))
+                .body("status", equalTo("PAID"))
+                .body("moment", equalTo("2022-07-25T13:00:00Z"))
+                .body("client.name",equalTo("Carolina de Luca"))
+                .body("payment.id", is(1))
+                .body("payment.moment", equalTo("2022-07-25T15:00:00Z"))
+                .body("items.productId", hasItems(1, 3))
+                .body("items.name", hasItems("The Lord of the Rings","Macbook Pro"))
+                .body("total", is(1431.0F));
+    }
+
 
 }

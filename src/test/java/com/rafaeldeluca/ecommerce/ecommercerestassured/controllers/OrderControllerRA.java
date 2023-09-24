46c52af1cf054223fba982d45298a3896049b6d4
Rafael de Luca <deluca1712@gmail.com>
2023-09-24 19:04:08 -0300

    test order by id - existing order id and admin logged

--- src/test/java/com/rafaeldeluca/ecommerce/ecommercerestassured/controllers/OrderControllerRA.java
+++ src/test/java/com/rafaeldeluca/ecommerce/ecommercerestassured/controllers/OrderControllerRA.java
@@ -1,10 +1,12 @@
 package com.rafaeldeluca.ecommerce.ecommercerestassured.controllers;
 
 import com.rafaeldeluca.ecommerce.ecommercerestassured.tests.BearerToken;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
-// import static io.restassured.matcher.RestAssuredMatchers.*;
+ import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 public class OrderControllerRA {
 
@@ -13,7 +15,7 @@ public class OrderControllerRA {
     private String clientToken, adminToken, invalidToken;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
 
         baseURI = "http://localhost:8080";
 
@@ -22,11 +24,31 @@ public class OrderControllerRA {
 
         clientUsername = "carolina@gmail.com";
         adminUsername = "rafaeldeluca@gmail.com";
-        clientPassword = adminPassword= "123456";
+        clientPassword = adminPassword = "123456";
 
         clientToken = BearerToken.obtainBearerToken(clientUsername, clientPassword);
         adminToken = BearerToken.obtainBearerToken(adminUsername, adminPassword);
         invalidToken = clientToken + "invalidString";
+    }
+
+    @Test
+    void findByIdShouldReturnOrderWhenOrderIdExistsAndUserLoggedAsAdmin () {
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .accept(ContentType.JSON)
+        .when()
+                .get("/orders/{id}", existingOrderId)
+        .then()
+                .statusCode(200)
+                .body("id", is(1))
+                .body("status", equalTo("PAID"))
+                .body("moment", equalTo("2022-07-25T13:00:00Z"))
+                .body("client.name",equalTo("Carolina de Luca"))
+                .body("payment.id", is(1))
+                .body("payment.moment", equalTo("2022-07-25T15:00:00Z"))
+                .body("items.productId", hasItems(1, 3))
+                .body("total", is(1431.0F));
     }
 
 

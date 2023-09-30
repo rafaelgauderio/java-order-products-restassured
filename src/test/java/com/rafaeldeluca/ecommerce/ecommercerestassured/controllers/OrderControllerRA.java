46f3b349ba3c801f37b58d798ba0d6c889e50dca
Rafael de Luca <deluca1712@gmail.com>
2023-09-30 18:43:18 -0300

    test order by id - non existing order id - logged as admin or client

--- src/test/java/com/rafaeldeluca/ecommerce/ecommercerestassured/controllers/OrderControllerRA.java
+++ src/test/java/com/rafaeldeluca/ecommerce/ecommercerestassured/controllers/OrderControllerRA.java
@@ -6,8 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
- import static io.restassured.matcher.RestAssuredMatchers.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
+
 public class OrderControllerRA {
 
     private Long existingOrderId, nonExistingOrderID, adminOrderId;
@@ -33,19 +34,19 @@ public class OrderControllerRA {
     }
 
     @Test
-    void findByIdShouldReturnOrderWhenOrderIdExistsAndUserLoggedAsAdmin () {
+    void findByIdShouldReturnOrderWhenOrderIdExistsAndUserLoggedAsAdmin() {
         given()
                 .header("Content-type", "application/json")
                 .header("Authorization", "Bearer " + adminToken)
                 .accept(ContentType.JSON)
-        .when()
+                .when()
                 .get("/orders/{id}", existingOrderId)
-        .then()
+                .then()
                 .statusCode(200)
                 .body("id", is(1))
                 .body("status", equalTo("PAID"))
                 .body("moment", equalTo("2022-07-25T13:00:00Z"))
-                .body("client.name",equalTo("Carolina de Luca"))
+                .body("client.name", equalTo("Carolina de Luca"))
                 .body("payment.id", is(1))
                 .body("payment.moment", equalTo("2022-07-25T15:00:00Z"))
                 .body("items.productId", hasItems(1, 3))
@@ -53,7 +54,7 @@ public class OrderControllerRA {
     }
 
     @Test
-    void findByIdShouldReturnOrderWhenOrderIdExistsAndUserLoggedAsClientAndOrderBelowsToClient () {
+    void findByIdShouldReturnOrderWhenOrderIdExistsAndUserLoggedAsClientAndOrderBelowsToClient() {
         given()
                 .header("Content-type", "application/json")
                 .header("Authorization", "Bearer " + clientToken)
@@ -65,25 +66,48 @@ public class OrderControllerRA {
                 .body("id", is(1))
                 .body("status", equalTo("PAID"))
                 .body("moment", equalTo("2022-07-25T13:00:00Z"))
-                .body("client.name",equalTo("Carolina de Luca"))
+                .body("client.name", equalTo("Carolina de Luca"))
                 .body("payment.id", is(1))
                 .body("payment.moment", equalTo("2022-07-25T15:00:00Z"))
                 .body("items.productId", hasItems(1, 3))
-                .body("items.name", hasItems("The Lord of the Rings","Macbook Pro"))
+                .body("items.name", hasItems("The Lord of the Rings", "Macbook Pro"))
                 .body("total", is(1431.0F));
     }
 
     @Test
-    void findByIdShouldReturnOrderWhenIdExistsAndUserLoggedAsClientAndOrderBelongsToClientOtherClient () {
+    void findByIdShouldReturnOrderWhenIdExistsAndUserLoggedAsClientAndOrderBelongsToClientOtherClient() {
 
         given()
-         .header("Content-type", "application/json")
+                .header("Content-type", "application/json")
                 .header("Authorization", "Bearer " + clientToken)
                 .accept(ContentType.JSON)
-                .when()
+        .when()
                 .get("/orders/{id}", adminOrderId)
-                .then()
+        .then()
                 .statusCode(403);
+    }
+    @Test
+    void findByIdShouldReturnNotFoundWhenIdDoesNotExistsAndUserLoggedAsClient () {
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + clientToken)
+                .accept(ContentType.JSON)
+        .when()
+                .get("/orders{id}" ,nonExistingOrderID)
+         .then()
+                .statusCode(404); // Not Found
+    }
+
+    @Test
+    void findByIdShouldReturnNotFoundWhenIdDoesNotExistsAndUserLoggedAsAdmin () {
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/orders/{id}" ,nonExistingOrderID)
+                .then()
+                .statusCode(404); // Not Found
     }
 
 
